09f0f59143d0ba1b0eb326bc50ab1bf1b23f84a3
CameronRuch <cameron.ruch@yahoo.com>
2022-04-14 00:20:04 -0500

    completed more middleware functions

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,33 +1,59 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
-  console.log(` [${timestamp}] ${method} to ${url}`)
+  console.log(`[${timestamp}] ${method} to ${url}`)
   next()
-  
+
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUserId middleware')
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'no such user'
+      })
+    } else {
+      req.user = user;
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error finding user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUser middleware')
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validatePost middleware')
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'missing text field'
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
-
-// do not forget to expose these functions to other modules
 
 module.exports = {
   logger,
   validateUserId,
   validateUser,
-  validatePost
+  validatePost,
 }
